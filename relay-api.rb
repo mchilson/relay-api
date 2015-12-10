d0608315142c03adb0dcba3b3bafb19768b2136d
mchilson <emailme@mikechilson.com>
2015-12-10 13:58:17 -0600

    Added error trapping

--- relay-api.rb
+++ relay-api.rb
@@ -1,35 +1,67 @@
 require 'sinatra'
 require 'sinatra/config_file'
 require 'json'
+require 'pi_piper'
+
+include PiPiper
 
 #read the config file and set the port
 config_file 'config.yml'
 set :port, settings.port
+set :bind, settings.ip
+
+$pin = PiPiper::Pin.new(:pin => 2, :direction => :out)
 
 # Specify the authorization logic
 use Rack::Auth::Basic, 'Authorization Required' do |username, password|
   username == 'admin' and password == 'admin'
 end
 
+
+
 #protected pages
 get '/' do
-  'Nothing to see here - Move along!'
+  '<h2>Relay API</h2>
+   <p><b>/on</b> - trigger the relay to the "on" position</p>
+   <p><b>/off</b> - trigger the relay to the "off" position</p>
+   <p><b>/status</b> - returns the current status of the relay (1 or 0)</p>'
 end
 
 get '/on' do
     #set the relay to on and send success or failure message
-    content_type :json
-    { :success => true, :message => '' }.to_json
+    begin
+        $pin.on
+    rescue
+    	content_type :json
+    	{ :success => false, :message => 'A GPIO Error Occured' }.to_json
+    else
+    	content_type :json
+    	{ :success => true, :message => 'Success' }.to_json
+    end        
 end
 
 get '/off' do
     #set the relay to off and send success or failure message
-    content_type :json
-    { :success => true, :message => '' }.to_json
+    begin
+        $pin.off
+    rescue
+    	content_type :json
+    	{ :success => false, :message => 'A GPIO Error Occured' }.to_json
+    else
+        content_type :json
+        { :success => true, :message => 'Success' }.to_json
+    end
 end
 
 get '/status' do
     #get the relay status and return it
-    content_type :json
-    { :state => 1, :message => '' }.to_json
+    begin
+    	$pin.read
+    rescue
+        content_type :json
+        { :status => 0, :message => 'A GPIO Error has Occured' }.to_json
+    else
+        content_type :json
+        { :state => $pin.value, :message => 'Success' }.to_json
+    end
 end
